75532a0e3db0d8f0ad002b64539cdf6dbc1d8733
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-07-01 00:50:54 +0530

    Add: Temporary Cards to show products

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import { Loader, Card, FormField } from '../components';
 
+const RenderCards = ({ data, title }) => {
+  if (data?.length > 0) {
+    return data.map((post) => <Card key={post._id} {...post} />)
+  }
+  return (
+    <h2 className='mt-5 font-bold text-[#694faf] text-xl uppercase'>{title}
+    </h2>
+  )
+}
+
 const Home = () => {
   const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState([null]);
-  const [searchProduct, setSearchProduct] = useState('asdf');
+  const [searchProduct, setSearchProduct] = useState('');
 
   return (
     <section className='max-w-7xl mx-auto'>
@@ -28,11 +38,17 @@ const Home = () => {
           </div>
         ) : (
           <>
-          {searchProduct && (
-            <h2 className='font-medium text-[#6c757d] text-[16px] mb-3'>
-              Showing results for <span className='text-[#343a40]'>{searchProduct}</span>
-            </h2>
-          )}
+            {searchProduct && (
+              <h2 className='font-medium text-[#6c757d] text-[16px] mb-3'>
+                Showing results for <span className='text-[#343a40]'>{searchProduct}</span>
+              </h2>
+            )}
+            <div className='grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3'>
+              {searchProduct ? (
+                <RenderCards data={[]} title="No search results found" />) : (
+                <RenderCards data={[]} title="No product found" />)
+              }
+            </div>
           </>
         )}
       </div>
