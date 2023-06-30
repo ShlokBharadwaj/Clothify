9062f741121a272292a2f265ddfebb81a0d2a04a
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-06-30 23:56:35 +0530

    Add: Routing for Home and Blend Post section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,12 +7,18 @@ import { Home, BlendPost } from './pages';
 const App = () => {
   return (
     <Router>
-      <header className='w-full flex justify-between items-center bg-[#343a40] sm:px-8 px-4 py-4 border-b border-b-[#e6e6e6]'>
+      <header className='w-full flex justify-between items-center bg-[#343a40] sm:px-8 p-4 border-b border-b-[#e6e6e6]'>
         <Link to='/'>
           <img src={logoPV} alt='logo' className='w-28 object-contain scale-125' />
         </Link>
-
+        <Link to='/blend-post' className='font-inter font-medium bg-[#694faf] text-white px-4 py-2 rounded-md'>Blend</Link>
       </header>
+      <main className='sm:p-8 px-4 py-8 w-full bg-slate-100 min-h-[calc(100vh-73px)]'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/blend-post' element={<BlendPost />} />
+        </Routes>
+      </main>
     </Router> 
   );
 };
