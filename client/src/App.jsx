cdedb06cf2b209dfed6a43587c076c7c3e3e837c
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-06-30 23:44:40 +0530

    Update: Persist Ventures logo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
 
+import { logo,logoPV } from './assets';
 import { Home, BlendPost } from './pages';
-import logo from './assets';
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold">
-      Hello world!
-    </h1>
-  )
-}
+    <Router>
+      <header className='w-full flex justify-between items-center bg-[#343a40] sm:px-8 px-4 py-4 border-b border-b-[#e6e6e6]'>
+        <Link to='/'>
+          <img src={logoPV} alt='logo' className='w-28 object-contain scale-125' />
+        </Link>
 
-export default App
+      </header>
+    </Router> 
+  );
+};
+
+export default App;
--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,9 +1,11 @@
 import download from './download.png';
 import logo from './logo.svg';
 import preview from './preview.png';
+import logoPV from './logo_pv.png';
 
 export {
   download,
   logo,
   preview,
+  logoPV,
 };
