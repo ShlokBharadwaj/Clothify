b60f403d74f464f7a9727cf965b8e15327ae6a26
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-07-01 00:18:12 +0530

    Add: Description for Product Showcase

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
     <Router>
       <header className='w-full flex justify-between items-center bg-[#343a40] sm:px-8 p-4 border-b border-b-[#e6e6e6]'>
         <Link to='/'>
-          <img src={logoPV} alt='logo' className='w-28 object-contain scale-125' />
+          <img src={logoPV} alt='logo' className='w-28 ml-2 object-contain scale-125' />
         </Link>
         <Link to='/blend-post' className='font-inter font-medium bg-[#694faf] text-white px-4 py-2 rounded-md'>Blend</Link>
       </header>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Loader, Card, FormField } from '../components';
 
 const Home = () => {
+  const [loading, setLoading] = useState(false);
+  const [allPosts, setAllPosts] = useState([null]);
+
   return (
-    <div>Home</div>
+    <section className='max-w-7xl mx-auto'>
+      <div>
+        <h1 className='font-extrabold text-[#343a40] text-[32px]'>
+          Our Products Showcase
+        </h1>
+        <p className='mt-2 text-[#6c757d] text-[16px] max-w-[500px]'>
+          Discover your perfect fit with AI-powered virtual try-on.
+        </p>
+      </div>
+    </section>
   )
 }
 
