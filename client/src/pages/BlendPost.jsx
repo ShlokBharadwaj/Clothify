9365a4468a4414c76f6c745db67c950f35105844
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-07-01 11:23:41 +0530

    Add: State for when Images are being loaded from API

--- client/src/pages/BlendPost.jsx
+++ client/src/pages/BlendPost.jsx
@@ -6,6 +6,15 @@ import { getRandomPrompts } from '../utils';
 import { FormField, Loader } from '../components';
 
 const BlendPost = () => {
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: '',
+    prompt: '',
+    photo:'',
+  });
+  const [generatingImg, setgeneratingImg] = useState(false);
+  const [loading, setloading] = useState(false);
+
   return (
     <div>BlendPost</div>
   )
