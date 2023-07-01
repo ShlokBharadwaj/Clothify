d3cf189cbfe890c9daf48b51a8a3273388b22f4f
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-07-01 12:58:49 +0530

    Add: Generic FormField

--- client/src/pages/BlendPost.jsx
+++ client/src/pages/BlendPost.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { preview } from '../assets';
@@ -10,13 +10,47 @@ const BlendPost = () => {
   const [form, setForm] = useState({
     name: '',
     prompt: '',
-    photo:'',
+    photo: '',
   });
   const [generatingImg, setgeneratingImg] = useState(false);
   const [loading, setloading] = useState(false);
+  const handleSubmit = () => {}
+  const handleChange = (e) => {}
+  const handleSuggestMe = () => {}
 
   return (
-    <div>BlendPost</div>
+    <section className='max-w-7xl mx-auto'>
+      <div>
+        <h1 className='font-extrabold text-[#343a40] text-[32px]'>
+          Blend
+        </h1>
+        <p className='mt-2 text-[#6c757d] text-[16px] max-w-[500px]'>
+          Elevate your style with DALL-E & MidJourney's AI-powered virtual try-on for the perfect fit
+        </p>
+      </div>
+      <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
+        <div className='flex flex-col gap-5'>
+          <FormField
+            LabelName='Your Name'
+            type='text'
+            name='name'
+            placeholder='Lorem Ipsum'
+            value={form.name}
+            handleChange={handleChange}
+          ></FormField>
+          <FormField
+            LabelName='prompt'
+            type='text'
+            name='prompt'
+            placeholder='a man trying on a casual outfit for a casual gathering'
+            value={form.prompt}
+            handleChange={handleChange}
+            isSuggestMe
+            handleSuggestMe={handleSuggestMe}
+          ></FormField>
+        </div>
+      </form>
+    </section>
   )
 }
 
