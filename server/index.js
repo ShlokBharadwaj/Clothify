0cf8de108dfbe064c41c91facf293623d4dca89c
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-07-02 00:06:26 +0530

    Add: Routes to middleware for frontend

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 
 import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
+import thenextlegRoutes from './routes/thenextlegRoutes.js';
 
 // import environment variables from dotenv file.
 dotenv.config();
@@ -10,6 +13,10 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
+
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
+app.use('/api/v1/thenextleg', thenextlegRoutes);
 
 app.get('/', async (req, res) => {
     res.send('Hello World!');
