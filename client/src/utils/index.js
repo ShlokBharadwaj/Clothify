5e364fa191dd918373262e0ec61cd68c2c588d82
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-07-01 11:19:12 +0530

    Add: Random prompts for user to generate random images

---
+++ client/src/utils/index.js
@@ -0,0 +1,9 @@
+import { suggestMePrompts } from '../constants';
+
+export function getRandomPrompts(prompt) {
+    const randomIndex = Math.floor(Math.random() * suggestMePrompts.length);
+    const randomPrompt = suggestMePrompts[randomIndex];
+    if(randomPrompt === prompt) return getRandoPrompt(prompt);
+
+    return randomPrompt;
+};
