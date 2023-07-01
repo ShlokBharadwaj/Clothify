9e1f069719e63b7077abaf6df19dd903e9d5e058
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-07-02 01:20:53 +0530

    Remove: Posts from dalle & tnl

--- server/routes/thenextlegRoutes.js
+++ server/routes/thenextlegRoutes.js
@@ -2,8 +2,6 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import { TNL } from 'tnl-midjourney-api';
 
-import Post from '../mongodb/models/post.js';
-
 dotenv.config();
 
 const router = express.Router();
