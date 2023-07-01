30b85006f9fd689627db658095d30ec7dec51347
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-07-01 15:22:22 +0530

    Remove: Border from FormField

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -14,7 +14,7 @@ const FormField = ({ LabelName, type, name, placeholder, value, handleChange, is
             className='font-semibold text-xs bg-slate-300 py-1 px-2 rounded-md text-black hover:animate-pulse'>Suggest Me</button>
         )}
       </div>
-      <input type={type} id={name} name={name} placeholder={placeholder} value={value} onChange={handleChange} required className='bg-gray-50 text-gray-900 text-sm rounded-md focus:ring-[#694faf] focus:border-[#694faf] outline-none block w-full p-3 shadow-xl' />
+      <input type={type} id={name} name={name} placeholder={placeholder} value={value} onChange={handleChange} required className='bg-gray-50 text-gray-900 text-sm rounded-md outline-none block w-full p-3 shadow-xl' />
     </div>
   )
 }
