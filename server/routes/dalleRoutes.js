082309bc82cd7b49c1dfaae79f200bf693d00078
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-07-02 01:56:25 +0530

    Add: OpenAI image editing endpoint to router

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -11,8 +11,26 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-router.route('/').get((req, res) => {
-    res.send('Hello World! from DALL-E!');
+router.route('/').post(async (req, res) => {
+    try {
+        const { prompt } = req.body;
+
+        const aiResponse = await openai.createImageEdit({
+            image: "",
+            prompt: { prompt },
+            mask: "",
+            n: 1,
+            size: "1024x1024",
+            responseFormat: "b64_json",
+        });
+
+        const image = aiResponse.data.data[0].b64_json;
+        res.status(200).json({ photo: image });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error?.response.data.error.message)
+    }
 });
 
 export default router;
