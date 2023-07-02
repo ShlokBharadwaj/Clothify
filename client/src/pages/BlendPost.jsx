2164116e38ec6a6836d0fcc824e2926052c6f709
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-07-02 10:13:11 +0530

    Add: prompt to backend

--- client/src/pages/BlendPost.jsx
+++ client/src/pages/BlendPost.jsx
@@ -22,7 +22,22 @@ const BlendPost = () => {
     const randomPrompt = getRandomPrompts();
     setForm({ ...form, prompt: randomPrompt });
   }
-  const generateImage = () => { }
+  const generateImage = async () => {
+    if (form.prompt) {
+      try {
+        setgeneratingImg(true);
+        const response = await fetch('http://localhost:8080/api/v1/dalle', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ prompt: form.prompt })
+        })
+      } catch (error) {
+
+      }
+    }
+  }
 
   return (
     <section className='max-w-7xl mx-auto'>
