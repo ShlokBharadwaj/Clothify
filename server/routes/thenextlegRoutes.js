4a40ea7ab4a0f14e955ab176d5ab27c91f45747f
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-07-22 20:19:47 +0530

    Update: Restore to default server

--- server/routes/thenextlegRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import { TNL } from 'tnl-midjourney-api';
-
-dotenv.config();
-
-const router = express.Router();
-
-export default router;
