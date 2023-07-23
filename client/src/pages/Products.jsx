f25a87ba277bff313ca57829422edd2d49d4b72b
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-07-23 17:56:30 +0530

    Add: Product images

--- client/src/pages/Products.jsx
+++ client/src/pages/Products.jsx
@@ -3,20 +3,23 @@ import React from 'react';
 const Products = () => {
     // TODO: Fetch data from products array with image URLs and descriptions
     const products = [
-        { imageUrl: '/path/to/image1.jpg', description: 'Product 1' },
-        { imageUrl: '/path/to/image2.jpg', description: 'Product 2' },
-        { imageUrl: '/path/to/image3.jpg', description: 'Product 3' },
-        { imageUrl: '/path/to/image4.jpg', description: 'Product 4' },
-        { imageUrl: '/path/to/image5.jpg', description: 'Product 5' },
-        { imageUrl: '/path/to/image6.jpg', description: 'Product 6' },
+        { imageUrl: 'http://localhost:8000/products/img1.png', description: 'Yellow Winter Coat' },
+        { imageUrl: 'http://localhost:8000/products/img2.png', description: 'Blue Denim Jeans' },
+        { imageUrl: 'http://localhost:8000/products/img3.png', description: 'Gray Business Suit' },
+        { imageUrl: 'http://localhost:8000/products/img4.png', description: 'White Shirt' },
+        { imageUrl: 'http://localhost:8000/products/img5.png', description: 'Blue Crop Top' },
+        { imageUrl: 'http://localhost:8000/products/img6.png', description: 'Green T-Shirt' },
+        { imageUrl: 'http://localhost:8000/products/img7.png', description: 'Red Skirt' },
+        { imageUrl: 'http://localhost:8000/products/img8.png', description: 'Red&White Checked Shirt' },
+        { imageUrl: 'http://localhost:8000/products/img9.png', description: 'White Tuxedo' },
     ];
 
     return (
         <div className="grid sm:grid-cols-3 grid-cols-2 gap-4 mt-8">
             {products.map((product, index) => (
-                <div key={index} className="border border-gray-300 p-4 rounded-lg">
-                    <img src={product.imageUrl} alt={product.description} className="w-full h-64 object-cover mb-2 rounded-lg" />
-                    <p className="text-gray-800 text-sm">{product.description}</p>
+                <div key={index} className="border border-gray-300 p-4 rounded-lg cursor-pointer">
+                    <img src={product.imageUrl} alt={product.description} className="w-full h-64 sm:object-contain  object-cover mb-2 rounded-lg" />
+                    <p className="text-gray-800 text-sm text-center font-semibold">{product.description}</p>
                 </div>
             ))}
         </div>
