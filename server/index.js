e9d40763c7f70098d900a434dcdc0e96cb4d112f
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-07-24 14:13:25 +0530

    Remove: Unused imports from index.js

--- server/index.js
+++ server/index.js
@@ -19,8 +19,6 @@ const upload = multer({ storage });
 
 app.use(express.urlencoded({ extended: false }));
 app.use('/products', express.static(path.join(__dirname, 'products')));
-
-
 app.use(cors());
 
 // routes
