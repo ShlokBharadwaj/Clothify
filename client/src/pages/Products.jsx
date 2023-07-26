75cab3061b88332bf66164171de04f5ae102c94e
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-07-27 00:29:11 +0530

    Update: Product URLs from localhost -> Cloudinary

--- client/src/pages/Products.jsx
+++ client/src/pages/Products.jsx
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Products = () => {
-    // TODO: Fetch data from products array with image URLs and descriptions
+const Products = ({ setSelectedProduct }) => {
+
+    const chooseImage = (product) => {
+        setSelectedProduct(product);
+    };
+
     const products = [
-        { imageUrl: 'http://localhost:8000/products/img8.png', description: 'Yellow Winter Coat' },
-        { imageUrl: 'http://localhost:8000/products/img2.png', description: 'Blue Denim Jeans' },
-        { imageUrl: 'http://localhost:8000/products/img3.png', description: 'Gray Business Suit' },
-        { imageUrl: 'http://localhost:8000/products/img9.png', description: 'White Shirt' },
-        { imageUrl: 'http://localhost:8000/products/img4.png', description: 'Green T-Shirt' },
-        { imageUrl: 'http://localhost:8000/products/img5.png', description: 'Red Skirt' },
-        { imageUrl: 'http://localhost:8000/products/img6.png', description: 'Red&White Checked Shirt' },
-        { imageUrl: 'http://localhost:8000/products/img1.png', description: 'Blue Crop Top' },
-        { imageUrl: 'http://localhost:8000/products/img7.png', description: 'White Tuxedo' },
+        { imageUrl: 'https://res.cloudinary.com/dhpintvvt/image/upload/v1690310722/Persist-Ventures/products/img8_atrttb.png', description: 'Yellow Winter Coat' },
+        { imageUrl: 'https://res.cloudinary.com/dhpintvvt/image/upload/v1690310727/Persist-Ventures/products/img2_jfwvsu.png', description: 'Blue Denim Jeans' },
+        { imageUrl: 'https://res.cloudinary.com/dhpintvvt/image/upload/v1690310723/Persist-Ventures/products/img3_ajoo6n.png', description: 'Gray Business Suit' },
+        { imageUrl: 'https://res.cloudinary.com/dhpintvvt/image/upload/v1690310721/Persist-Ventures/products/img9_pmcb8a.png', description: 'White Shirt' },
+        { imageUrl: 'https://res.cloudinary.com/dhpintvvt/image/upload/v1690310715/Persist-Ventures/products/img4_a2ztbp.png', description: 'Green T-Shirt' },
+        { imageUrl: 'https://res.cloudinary.com/dhpintvvt/image/upload/v1690310716/Persist-Ventures/products/img5_kku1x8.png', description: 'Red Skirt' },
+        { imageUrl: 'https://res.cloudinary.com/dhpintvvt/image/upload/v1690310717/Persist-Ventures/products/img6_eav1nc.png', description: 'Red&White Checked Shirt' },
+        { imageUrl: 'https://res.cloudinary.com/dhpintvvt/image/upload/v1690310724/Persist-Ventures/products/img1_hkldan.png', description: 'Blue Crop Top' },
+        { imageUrl: 'https://res.cloudinary.com/dhpintvvt/image/upload/v1690310722/Persist-Ventures/products/img7_gdlddq.png', description: 'White Tuxedo' },
     ];
+
 
     return (
         <div>
@@ -25,8 +30,16 @@ const Products = () => {
 
             <div className="grid sm:grid-cols-3 grid-cols-2 gap-4 mt-8">
                 {products.map((product, index) => (
-                    <div key={index} className="border border-gray-300 p-4 rounded-lg cursor-pointer">
-                        <img src={product.imageUrl} alt={product.description} className="w-full h-64 sm:object-contain  object-cover mb-2 rounded-lg" />
+                    <div
+                        key={index}
+                        className="border border-gray-300 p-4 rounded-lg cursor-pointer"
+                        onClick={() => setSelectedProduct(product)}
+                    >
+                        <img
+                            src={product.imageUrl}
+                            alt={product.description}
+                            className="w-full h-64 sm:object-contain object-cover mb-2 rounded-lg"
+                        />
                         <p className="text-gray-800 text-sm text-center font-semibold">{product.description}</p>
                     </div>
                 ))}
