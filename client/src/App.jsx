2dfaf87506dfe164d69937463d190d5af2389d69
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-08-03 20:48:50 +0530

    Update: Hard Reset branch before Vercel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
 
-import { logo,logoPV } from './assets';
+import { logo, logoPV } from './assets';
 import { Home, BlendPost } from './pages';
 
 const App = () => {
@@ -19,7 +19,7 @@ const App = () => {
           <Route path='/blend-post' element={<BlendPost />} />
         </Routes>
       </main>
-    </Router> 
+    </Router>
   );
 };
 
--- server/index.js
+++ server/index.js
@@ -3,25 +3,19 @@ const multer = require("multer");
 const cors = require("cors");
 const path = require("path");
 const fs = require("fs");
-const WebSocket = require("websocket");
 
-// const denv = require("dotenv").config();
+const denv = require("dotenv").config();
 
-// if (denv.error) {
-//     throw denv.error;
-// }
+if (denv.error) {
+    throw denv.error;
+}
 
 // console.log(denv.parsed);
 
 let readAndConvertImageFiles = require('./routes/blend.js').readAndConvertImageFiles;
 
 const app = express();
-
-// Specify the allowed origin explicitly
-app.use(cors({
-    origin: 'https://clothify-client.vercel.app',
-}));
-
+app.use(cors());
 const PORT = process.env.PORT || 8000;
 
 const storage = multer.diskStorage({
@@ -38,36 +32,6 @@ const upload = multer({ storage: storage });
 app.use(express.urlencoded({ extended: false }));
 app.use('/products', express.static(path.join(__dirname, 'products')));
 
-// WebSocket server
-const httpServer = require('http').createServer(app);
-const wsServer = new WebSocket.server({ httpServer });
-
-// Store the connected WebSocket clients
-const clients = [];
-
-// Handle the WebSocket clients
-wsServer.on("request", (request) => {
-    const connection = request.accept(null, request.origin);
-
-    // Add the connection to the clients array
-    clients.push(connection);
-
-    // Remove the connection from the clients array when it is closed
-    connection.on("close", () => {
-        const index = clients.indexOf(connection);
-        if (index !== -1) {
-            clients.splice(index, 1);
-        }
-    });
-});
-
-// Broadcast message to all connected clients
-function broadcastMessage(message) {
-    clients.forEach((client) => {
-        client.sendUTF(JSON.stringify(message));
-    });
-}
-
 // routes
 app.get("/", (req, res) => {
     res.send("<h1>Express Server</h1>");
@@ -75,26 +39,17 @@ app.get("/", (req, res) => {
 
 const directoryPath = path.join(__dirname, 'routes');
 
-fs.readdir(directoryPath, (err, files) => {
-    if (err) {
-        console.error('Error reading directory:', err);
-    } else {
-        console.log('Files in the directory:', files);
-    }
-});
-
 app.get('/api/response', (req, res) => {
     // Read the response.json file and send it as the response
     const responseJsonPath = path.join(directoryPath, "response.json");
-    console.log("The response JSON file path is: " + responseJsonPath);
+    console.log(responseJsonPath);
     const responseJsonData = fs.readFileSync(responseJsonPath, 'utf8');
     res.json(JSON.parse(responseJsonData));
 });
 
 app.post("/upload", upload.single("image"), (req, res) => {
     //    console.log(req);
-    // const imageUrl = `http://localhost:${PORT}/uploads/${req.file.filename}`;
-    const imageUrl = `${process.env.VERCEL_URL}/uploads/${req.file.filename}`;
+    const imageUrl = `http://localhost:${PORT}/uploads/${req.file.filename}`;
     // Respond with the image URL
     res.json({ imageUrl });
 });
@@ -109,16 +64,10 @@ app.post('/api/process-images', upload.single('userImage'), async (req, res) => 
         console.log('The user Image is: ' + userImage + ' And the Product image is: ' + productImage);
 
         res.json({ message: 'Image processing and response.json creation successful.' });
-
-        // After processing the images and creating response.json, send the updated data to all connected clients
-        const responseJsonPath = path.join(directoryPath, "response.json");
-        const responseJsonData = fs.readFileSync(responseJsonPath, 'utf8');
-        broadcastMessage(JSON.parse(responseJsonData));
     } catch (error) {
         console.error('Error processing images:', error);
         res.status(500).json({ error: 'Image processing failed.' });
     }
 });
 
-// app.listen(PORT, () => console.log(`Server running on localhost:${PORT}`));
-app.listen(PORT, () => console.log(`Server running on Vercel URL: ${process.env.VERCEL_URL}`));
+app.listen(PORT, () => console.log(`Server running on localhost:${PORT}`));
